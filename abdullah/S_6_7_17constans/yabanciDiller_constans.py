e90640f8b6a8234b27344fe1baa1859d4b9f70be
Abdullah KESKİN <apokeskin2807@gmail.com>
2024-05-04 00:10:26 +0300

    klasörler_olusturuldu

---
+++ abdullah/S_6_7_17constans/yabanciDiller_constans.py
@@ -0,0 +1,29 @@
+BASE_URL="https://tobeto.com/giris"
+MAIL="apokeskin2807@gmail.com"
+PASSWORD="A1p2o3.."
+USER_INPUT="email"
+PASWORD_INPUT="password"
+LOGIN_BUTTON_XPATH='//*[@id="__next"]/div/main/section/div/div/div/div/form/button'
+POPUP_XPATH="//div[@id='__next']/div[@class='back-white']//div[@role='alert']//button[@type='button']"
+KISI_BUTTON_CSS=".mb-0.name"
+PROFIL_BILGILERI_CSS='li:nth-of-type(1) > .dropdown-item.profil-dropdown'
+DIL_BUTTON_XPATH="//div[@id='__next']/div[@class='back-white']/main/section/div[@class='container']//div[@class='col-12 col-lg-3 mb-8 mb-lg-0']/div/a[7]/span[@class='sidebar-text']"
+FRANSIZCA_PAGE_BUTTON_XPATH='//*[@id="__next"]/div/main/section/div/div/div[2]/form/div/div[1]/select/option[8]'
+SEVIYE_BUTTON_XPATH='//*[@id="__next"]/div/main/section/div/div/div[2]/form/div/div[2]/select'
+ORTA_BUTTON_XPATH='//*[@id="__next"]/div/main/section/div/div/div[2]/form/div/div[2]/select/option[3]'
+KAYDET_XPATH='//*[@id="__next"]/div/main/section/div/div/div[2]/form/button'
+EKLE_MESAJ_XPATH='//*[@id="__next"]/div/div[2]/div/div[2]'
+EKLE_MESAJ_TEXT="• Yabancı dil bilgisi eklendi."
+DIL_BUTTON_XPATH="//div[@id='__next']/div[@class='back-white']/main/section/div[@class='container']//div[@class='col-12 col-lg-3 mb-8 mb-lg-0']/div/a[7]/span[@class='sidebar-text']"
+ALMANCA_PAGE_BUTTON_XPATH='//*[@id="__next"]/div/main/section/div/div/div[2]/form/div/div[1]/select/option[2]'
+SILME_KUTUSU_XPATH="/html//div[@id='__next']/div[@class='back-white']/main/section//div[@class='col-12 col-lg-9']/div/div/div[1]"
+SIL_POPUP_XPATH="/html//div[@id='__next']/div[@class='back-white']/main/section//div[@class='col-12 col-lg-9']/div/div/div[1]/span[@class='delete-lang']"
+EVET_XPATH="/html/body/div[@role='dialog']/div[@class='modal-dialog modal-dialog-centered']//button[@class='btn btn-yes my-3']"
+SILME_MESAJ_XPATH='//*[@id="__next"]/div/div[2]/div/div[2]'
+EKLEME_XPATH='//*[@id="__next"]/div/div[2]/div/div[2]'
+EKLEME_TEXT="• Bu dil zaten mevcut."
+DIL_ZORUNLU_XPATH='//*[@id="__next"]/div/main/section/div/div/div[2]/form/div/div[1]/p'
+SILME_MESAJ_TEXT="• Yabancı dil kaldırıldı."
+DIL_ZORUNLU_TEXT="Doldurulması zorunlu alan*"
+SEVIYE_ZORUNLU_TEXT="Doldurulması zorunlu alan*"
+SEVIYE_ZORUNLU_XPATH='//*[@id="__next"]/div/main/section/div/div/div[2]/form/div/div[2]/p'
