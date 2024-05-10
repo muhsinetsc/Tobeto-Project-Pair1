7d69dc3a7ea9d97836217dd4685ae4aab4b28641
GÜL KARAMAHMUTOGLU <krmoglugul@gmail.com>
2024-05-10 17:05:13 +0300

    senaryo 01 10 14 15

--- scenarios/SO01-record/test_unverified_account.py
+++ scenarios/SO01-record/test_unverified_account.py
@@ -2,12 +2,9 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.support.wait import WebDriverWait
 from constantsS01_10_14_15.globalConstants import *
-from time import sleep
 import pytest
 
 options = Options()
@@ -15,7 +12,7 @@ options.add_argument("start-maximized")
 class Test_Tobeto_Login:
     def setup_method(self, method):
         self.driver=webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-        self.driver.get(baseUrl)
+        self.driver.get(BASEURL)
         self.driver.maximize_window()      
   
     def teardown_method(self, method):
@@ -25,24 +22,19 @@ class Test_Tobeto_Login:
         return WebDriverWait(self.driver,timeout).until(ec.visibility_of_element_located(locator))    
            
         #Doğrulanmamış hesap (CASE2)
-    def test_account_verification(self):
-         
-         record_email= self.WaitForElementVisible((By.NAME, email_name))
-         record_password=self.WaitForElementVisible((By.NAME,password_name))
-         record_email.send_keys("pair1.ttobeto@gmail.com")  
-         record_password.send_keys("67hdS165")                                                                               
-         login_button=self.WaitForElementVisible((By.CSS_SELECTOR, login_button_selector))
+    def test_account_verification(self):         
+         record_email= self.WaitForElementVisible(EMAIL_NAME)
+         record_password=self.WaitForElementVisible(PASSWORD_NAME)
+         record_email.send_keys(RECORD_EMAILL)  
+         record_password.send_keys(RECORDPASSWORDD)                                                                               
+         login_button=self.WaitForElementVisible(LOGIN_BUTTON_SELECTOR)
          login_button.click()
-         alert_message=self.WaitForElementVisible((By.XPATH, alert_message_xpath))
-         alertMessage= self.WaitForElementVisible((By.CSS_SELECTOR, alertMessage_selector ))
-         
-         assert {alert_message.text == "Hesabınız henüz doğrulanmamış." and alertMessage.text ==  "Hesabınız henüz doğrulanmamış. Lütfen pair1.tobetoo@gmail.com adresinize ilettiğimiz doğrulama linkine tıklayın. Eğer doğrulama e-postası almadıysanız yeniden gönderebilmemiz için tıklayınız."}
-         sleep(3)
+         alert_message_account=self.WaitForElementVisible(ALERT_MESSAGE_XPATH)
+         alertMessage_account= self.WaitForElementVisible(ALERTMESSAGE_SELECTOR)         
+         assert {alert_message_account.text == ALERT_MESSAGE_ACCOUNT and alertMessage_account.text == ALERTMESSAGE_ACCOUNT}
+        
 
          
          
    
-  
 
-"""testclass = Test_Tobeto_Giris()
-testclass.test_account_verification()"""
--- scenarios/SO10-business_success_model/test_business_success_model.py
+++ scenarios/SO10-business_success_model/test_business_success_model.py
@@ -1,15 +1,11 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
-from trio import ConditionStatistics
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.support.wait import WebDriverWait
 from constantsS01_10_14_15.globalConstants import *
-from datetime import datetime
-from time import sleep
 import pytest
 
 options = Options()
@@ -17,9 +13,9 @@ options.add_argument("start-maximized")
 class Test_Business_Success_Model():
     def setup_method(self, method):
          self.driver=webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-         self.driver.get(baseUrl) 
-         self.driver.maximize_window()  
-         self.vars = {}
+         self.driver.get(BASEURL) 
+         self.driver.maximize_window() 
+       
 
     def teardown_method(self, method):
          self.driver.quit()
@@ -31,59 +27,47 @@ class Test_Business_Success_Model():
         return WebDriverWait(self.driver,timeout).until(ec.visibility_of_element_located(locator))
        
     def pre_condition(self):
-        email = self.WaitForElementVisible((By.NAME, email_name))
-        password = self.WaitForElementVisible((By.NAME, password_name))
-        email.send_keys("pair1tobeto@gmail.com")
-        password.send_keys("456789")  
-        login = self.WaitForElementVisible((By.XPATH, login_xpath)) # ".btn-primary" sınıfına sahip düğmeye tıklar.
+        email = self.WaitForElementVisible(EMAIL_NAME)
+        password = self.WaitForElementVisible(PASSWORD_NAME)
+        email.send_keys(LOGIN_EMAIL)
+        password.send_keys(RECORD_PASSWORD)  
+        login = self.WaitForElementVisible(LOGIN_EMAIL) 
         login.click()
         #değerlendirmeler
-        reviews = self.WaitForElementVisible((By.XPATH, reviews_xpath))
+        reviews = self.WaitForElementVisible(REVIEWS_XPATH)
         reviews.click()
 
     def test_business_success_model(self):   
         self.pre_condition()           
     # Raporu görüntüle düğmesini bekleyerek tıklama      
-        view_report = self.WaitForElementVisible((By.XPATH, view_report_xpath))
+        view_report = self.WaitForElementVisible(VIEW_REPORT_XPATH)
         view_report.click()
-        sleep(5)
         self.driver.execute_script("window.scrollBy(0,300)", "")
-        sleep(3) 
         # Ekran görüntüsünü bellekten al
         screenshot = self.driver.get_screenshot_as_png()
         # Ekran görüntüsünü dosyaya kaydet
         with open("screenshot.png", "wb") as f:
-             f.write(screenshot)
-       
-        graphic = self.WaitForElementVisible((By.XPATH,graphic_xpath)) # Sayfada ikinci sırada bulunan (By.CSS_SELECTOR, ".chartjs-render-monitor:nth-child(2)")))"
+             f.write(screenshot)       
+        graphic = self.WaitForElementVisible(GRAPHIC_XPATH) 
         graphic.click()
-        #ActionChains nesnesi oluştur
         actions = ActionChains(self.driver)   
-        #Fare imlecini belirtilen öğenin üzerine getir
-        actions.move_to_element(graphic).perform()                      
-    
-        button = self.WaitForElementVisible((By.XPATH, button_xpath))
+        actions.move_to_element(graphic).perform()                  
+        button = self.WaitForElementVisible(BUTTON_XPATH)
         button.click()
-        sleep(5)
-
+        
     def test_improve_yourself(self):
          self.pre_condition()
-         view_report =self.WaitForElementVisible((By.XPATH, view_report_xpath))
+         view_report =self.WaitForElementVisible(VIEW_REPORT_XPATH)
          view_report.click()
          self.driver.execute_script("window.scrollBy(0,1500)")                     
-         title = self.WaitForElementVisible((By.XPATH, title_xpath))
+         title = self.WaitForElementVisible(TITLE_XPATH)
          self.driver.execute_script("arguments[0].scrollIntoView();", title)
-         sleep(5)
-         buttonn = self.WaitForElementVisible((By.XPATH,buttonn_xpath))
+         buttonn = self.WaitForElementVisible(BUTTONN_XPATH)
          self.clickElementByJS(buttonn)
         
   
     
      
-"""testclass =Test_Business_Success_Model()
-testclass. test_business_success_model()
-testclass.test_improve_yourself()"""
-
 
 
 
